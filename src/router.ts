ae9c5d52b62e5798221d37f1b809daedc9cd9d6e
chancet1982 <chancet1982@gmail.com>
2018-08-11 17:20:57 +0200

    added sensible routing defaults

--- src/router.ts
+++ src/router.ts
@@ -4,7 +4,8 @@ import Home from './views/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
+  mode: "history",
   routes: [
     {
       path: '/',
@@ -18,6 +19,38 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    // treat all other paths as unknown and redirect
+		{
+      path: '*',
+      redirect: '/'
     }
   ]
-})
+});
+
+/*const user = store.getters.user;*/
+
+//Auth guard for restricted access
+/*router.beforeEach((to, from, next) => {
+	if (to.matched.some(record => record.meta.requiresAuth)) {
+	  // this route requires auth, check if logged in
+	  // if not, redirect to login page.
+	  if (!user || isEmpty(user)) {
+			const payload = {
+				text:'You must be logged in!',
+				type:'error'
+			}
+			store.dispatch('setAlert', payload);
+			next({
+			  path: '/login',
+			  query: { redirect: to.fullPath } //not sure how we could access this redirect value but would be nice
+			})
+	  } else {
+			next()
+	  }
+	} else {
+	  next() // make sure to always call next()!
+	}
+});*/
+
+export default router
